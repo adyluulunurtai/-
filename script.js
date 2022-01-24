25527dd9163588a6e5234308460040206650639e
Nurtai <adyluulunurtai7@gmail.com>
2022-01-24 20:33:07 +0300

    use removeEventLisneter

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const btn = document.querySelector('button');
 //btn.onclick = function(){
   //  alert("click");
 //};
+let i = 0;
+const deleteElement = (e)=>{
+    console.log(e.target);
+    i++;
+    if(i==1){
+        btn.removeEventListener("click", deleteElement)
+    }
+};
 
-btn.addEventListener('click', () =>{
-    alert("click");
-});
-btn.addEventListener('click', ()=>{
-    alert("second click");
-})
+btn.addEventListener('click', deleteElement);
+
+
